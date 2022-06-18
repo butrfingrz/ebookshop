4aaffabd90a34af4155650dfa7182421042920fa
butrfingrz <butrfingrz@yahoo.com>
2022-06-18 16:23:19 -0700

    Added comments to SQL

--- ebookshop/shop_create.sql
+++ ebookshop/shop_create.sql
@@ -1,11 +1,27 @@
+--Andrea Fletcher
+--Module 6
+--Create database and tables for it
+
+--If the Shop databse exists already, delete it
 DROP DATABASE IF EXISTS `shop`;
+
+--create the shop database
 CREATE DATABASE `shop`;
+
+--Create table: categories
+--Key: category_id
 CREATE TABLE `shop`.`categories` (
   `category_id` int NOT NULL AUTO_INCREMENT,
   `category_name` varchar(70) NOT NULL, 
   PRIMARY KEY (`category_id`),
   KEY `category_id_key` (`category_id`)
   );
+ 
+--Create table: books  
+--Key:book_id
+--Constraints:
+--Don't let a category be assigned to a book that 
+--  does not exist in the category table
 CREATE TABLE `shop`.`books` (
   `book_id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(70) NOT NULL,
@@ -16,6 +32,9 @@ CREATE TABLE `shop`.`books` (
   KEY `book_id_key` (`book_id`),
   CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`)
 );
+
+--Create table: orders  
+--Key: order_id
 CREATE TABLE `shop`.`orders` (
   `order_id` bigint NOT NULL AUTO_INCREMENT,
   `delivery_name` varchar(70) NOT NULL,
@@ -26,6 +45,14 @@ CREATE TABLE `shop`.`orders` (
   PRIMARY KEY (`order_id`),
   KEY `order_id_key` (`order_id`)
 );
+
+--Create table: order_details
+--Key: id
+--Contraints:
+-- Don't let a book_id be entered into this table
+--   if it doesn't exist in books table  
+-- Dont let an order_id be entered into this table 
+--   if it doesn't exist in the orders table
 CREATE TABLE `shop`.`order_details` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `book_id` int NOT NULL,
--- ebookshop/shop_populate.sql
+++ ebookshop/shop_populate.sql
@@ -1,6 +1,14 @@
+--Andrea Fletcher
+--Module 6
+--Insert the information into the tables
+--DB: shop
+
+--Table: Categories
 INSERT INTO `shop`.`categories` (`category_id`, `category_name`) VALUES ('1', 'Web Development');
 INSERT INTO `shop`.`categories` (`category_id`, `category_name`) VALUES ('2', 'Science Fiction');
 INSERT INTO `shop`.`categories` (`category_id`, `category_name`) VALUES ('3', 'Historical Mysteries');
+
+--Table: Books
 INSERT INTO `shop`.`books` (`book_id`, `title`, `author`, `price`, `category_id`) VALUES ('1', 'MYSQL 8 Query Performance Tuning', 'Jesper Wisborg Krogh', '34.31', '1');
 INSERT INTO `shop`.`books` (`book_id`, `title`, `author`, `price`, `category_id`) VALUES ('2', 'JavaScript Next', 'Raju Gandhi', '36.70', '1');
 INSERT INTO `shop`.`books` (`book_id`, `title`, `author`, `price`, `category_id`) VALUES ('3', 'The Complete Robot', 'Isaac Asimov', '12.13', '2');
